c1c754365850e7b814bd6641cf2ae9079a487359
Volodymyr Voronkin <voronkinvladimir328@gmail.com>
2025-05-14 15:39:21 +0200

    33

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ export function middleware(req: NextRequest) {
     const country = req.headers.get('x-vercel-ip-country') || 'unknown';
     return new Response(`Hello! Your country is: ${country}`);
 }
-
 export const config = {
     matcher: ['/'],
 };
