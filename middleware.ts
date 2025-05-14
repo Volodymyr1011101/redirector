d2f263063f6c0099551d8a4fbe04c6b764f0944c
Volodymyr Voronkin <voronkinvladimir328@gmail.com>
2025-05-14 15:37:08 +0200

    .

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-    const country = req.headers.get('x-vercel-ip-country') || 'US';
-
-    const redirectUrl =
-        country === 'PL'
-            ? 'https://panorama-studio.pl'
-            : 'https://google.com';
-
-    return NextResponse.redirect(redirectUrl, 302);
+    const country = req.headers.get('x-vercel-ip-country') || 'unknown';
+    return new Response(`Hello! Your country is: ${country}`);
 }
 
-// Применяем middleware только к корню
 export const config = {
     matcher: ['/'],
 };
